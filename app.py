9a667a54d5277434c2d50842449f303b5a6fb77a
RyanVerhey <ryan90v@gmail.com>
2020-05-25 17:35:33 -0400

    Pseudocoding script

---
+++ app.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+START_DATE = datetime.datetime(1958, 8, 4)
+CHART_NAME = 'hot-100'
+
+
+if __name__ == '__main__':
+    pass
+    # Get list of all songs (by year) that appear in Billboard Hot 100 (no duplicats by year)
+    #   Save them in a database?
+    # Get lyrics for those songs
+    # Count the number of times the word 'love' appears in the lyrics and title
+    # Output CSV number of times 'love' appears for each year
