70b87e31983ab79d70b3202cb146eae0299b6201
RyanVerhey <ryan90v@gmail.com>
2020-05-28 21:12:17 -0400

    Finding if a song title or lyrics contain pattern

--- song.py
+++ song.py
@@ -1,3 +1,5 @@
+import re
+
 class Song:
     """Represents a song on the billboard Top 100
     Has an artist (required), title (required), lyrics (optional),
@@ -16,6 +18,15 @@ class Song:
     @property
     def artist(self):
         return self._artist
+
+    def occurs_in_title_or_lyrics(self, pattern):
+        """Takes a regex pattern and returns whether or not that pattern appears
+        in the title or lyrics"""
+        lyrics = self.lyrics or ''
+        matcher = re.compile(pattern, re.I)
+        title_match = bool(matcher.search(self._title))
+        lyric_match = bool(matcher.search(lyrics))
+        return title_match or lyric_match
 
     def __hash__(self):
         return hash((self._title, self._artist))
